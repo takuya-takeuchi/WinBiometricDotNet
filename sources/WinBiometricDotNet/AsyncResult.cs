2da9f22fe10b693c2292fc20f6a0df5ad9237dfd
Takuya Takeuchi <spitzbergen@gmail.com>
2018-08-16 08:20:52 +0900

    doc: add xml comment

--- sources/WinBiometricDotNet/AsyncResult.cs
+++ sources/WinBiometricDotNet/AsyncResult.cs
@@ -5,6 +5,9 @@ using HRESULT = System.Int32;
 namespace WinBiometricDotNet
 {
 
+    /// <summary>
+    /// The <see cref="AsyncResult"/> class contains the results of an asynchronous operation.
+    /// </summary>
     public sealed class AsyncResult
     {
 
@@ -59,41 +62,65 @@ namespace WinBiometricDotNet
 
         #region Properties
 
+        /// <summary>
+        /// Gets the error code returned by the operation.
+        /// </summary>
         public HRESULT ApiStatus
         {
             get;
         }
 
+        /// <summary>
+        /// Gets the handle of an asynchronous session started by calling the <see cref="WinBiometric.AsyncOpenFramework"/> function.
+        /// </summary>
         public Framework Framework
         {
             get;
         }
 
+        /// <summary>
+        /// Gets type of the asynchronous operation.
+        /// </summary>
         public OperationTypes OperationType
         {
             get;
         }
 
+        /// <summary>
+        /// Gets the sequence number of the asynchronous operation.
+        /// </summary>
         public ulong SequenceNumber
         {
             get;
         }
 
+        /// <summary>
+        /// Gets the handle of an asynchronous session started by calling the <see cref="WinBiometric.AsyncOpenSession"/> function.
+        /// </summary>
         public Session Session
         {
             get;
         }
 
+        /// <summary>
+        /// Gets the system date and time at which the biometric operation began.
+        /// </summary>
         public DateTime TimeStamp
         {
             get;
         }
 
+        /// <summary>
+        /// Gets the numeric unit identifier of the biometric unit that performed the operation.
+        /// </summary>
         public uint UnitId
         {
             get;
         }
 
+        /// <summary>
+        /// Gets the address of an optional buffer supplied by the caller.
+        /// </summary>
         public IntPtr UserData
         {
             get;
